c357343f708e976e9f54a431a7f2fa7c809701df
jnm2 <me@jnm2.com>
2023-08-12 10:09:08 -0400

    This has been part of .NET since 5

--- src/YouTubeDownloadTool/Utils/Extensions.cs
+++ src/YouTubeDownloadTool/Utils/Extensions.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Windows;
 using System.Windows.Interop;
 
@@ -24,77 +21,6 @@ namespace YouTubeDownloadTool
         public static WinErrorCode GetErrorCode(this Exception exception)
         {
             return (WinErrorCode)exception.HResult;
-        }
-
-        public static ProcessExitAwaitable WaitForExitAsync(this Process process)
-        {
-            return new ProcessExitAwaitable(process);
-        }
-
-        public readonly struct ProcessExitAwaitable
-        {
-            private readonly Process process;
-
-            public ProcessExitAwaitable(Process process)
-            {
-                this.process = process ?? throw new ArgumentNullException(nameof(process));
-            }
-
-            public ProcessExitAwaiter GetAwaiter() => new(process);
-
-            public readonly struct ProcessExitAwaiter : ICriticalNotifyCompletion
-            {
-                private readonly Process process;
-
-                public ProcessExitAwaiter(Process process)
-                {
-                    this.process = process ?? throw new ArgumentNullException(nameof(process));
-                }
-
-                public bool IsCompleted => process.HasExited;
-
-                public void OnCompleted(Action continuation)
-                {
-                    if (continuation is null) return;
-
-                    var closure = new OnCompletedClosure(continuation);
-
-                    process.EnableRaisingEvents = true;
-                    process.Exited += closure.OnProcessExited;
-                    if (process.HasExited) closure.InvokeContinuation();
-                }
-
-                private sealed class OnCompletedClosure
-                {
-                    private Action? continuation;
-
-                    public OnCompletedClosure(Action continuation)
-                    {
-                        this.continuation = continuation;
-                    }
-
-                    public void OnProcessExited(object? sender, EventArgs e)
-                    {
-                        InvokeContinuation();
-                    }
-
-                    public void InvokeContinuation()
-                    {
-                        Interlocked.Exchange(ref continuation, null)?.Invoke();
-                    }
-                }
-
-                public void UnsafeOnCompleted(Action continuation)
-                {
-                    OnCompleted(continuation);
-                }
-
-                public void GetResult()
-                {
-                    if (!process.HasExited)
-                        throw new InvalidOperationException("GetResult may only be called after IsCompleted returns true or OnCompleted invokes a callback.");
-                }
-            }
         }
 
         public static System.Windows.Forms.DialogResult ShowDialog(this System.Windows.Forms.CommonDialog dialog, Window owner)
